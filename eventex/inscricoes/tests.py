1b15452519d93091dd923ddade60511f5d1dde81
vicente.luz <vicenteoluz@gmail.com>
2018-06-24 15:50:22 -0300

    Realiza Inscricoes

--- eventex/inscricoes/tests.py
+++ eventex/inscricoes/tests.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.test import TestCase
 from eventex.inscricoes.forms import InscricaoForm
 
@@ -35,3 +36,67 @@ class InscricaoTest(TestCase):
         '''Form must have foour fields'''
         form = self.resp.context['form']
         self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
+
+
+class InscricaoPostValid(TestCase):
+    def setUp(self):
+        data = dict(name = 'Vicente Luz', cpf = '12345678901',
+                    email = 'vicente.luz@armazemparaiba.com.br',
+                    phone = '86-98822-1812')
+        self.resp = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+
+    def test_post(self):
+        '''Valid post redirect to /inscricao/'''
+        self.assertEqual(302, self.resp.status_code)
+
+    def test_send_inscricao_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_inscricao_email_subject(self):
+        expect = 'Confirmacao de Inscricao'
+        self.assertEqual(expect, self.email.subject)
+
+    def test_inscricao_email_from(self):
+        expect = 'contato@eventex.com'
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_inscricao_email_to(self):
+        expect = ['contato@eventex.com', 'vicente.luz@armazemparaiba.com.br']
+        self.assertEqual(expect, self.email.to)
+
+    def test_inscricao_email_body(self):
+        self.assertIn('Vicente Luz', self.email.body)
+        self.assertIn('12345678901', self.email.body)
+        self.assertIn('vicente.luz@armazemparaiba.com.br', self.email.body)
+        self.assertIn('86-98822-1812', self.email.body)
+
+
+class InscricaoPostInvalid(TestCase):
+    def setUp(self):
+        data = dict()
+        self.resp = self.client.post('/inscricao/', data)
+        self.form = self.resp.context['form']
+
+    def test_post(self):
+        '''Invalid post should not redirect'''
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'inscricao_form.html')
+
+    def test_has_form(self):
+        self.assertIsInstance(self.form, InscricaoForm)
+
+    def test_form_has_errors(self):
+        self.assertTrue(self.form.errors)
+
+class InscricaoSuccessMessage(TestCase):
+    def test_message(self):
+        data = dict(name='Vicente Luz', cpf='12345678901',
+                    email='vicente.luz@armazemparaiba.com.br',
+                    phone='86-98822-1812')
+        response = self.client.post('/inscricao/', data, follow=True)
+        self.assertContains(response, 'Inscrição realizada com sucesso!')
+
--- eventex/inscricoes/views.py
+++ eventex/inscricoes/views.py
@@ -1,8 +1,30 @@
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 from eventex.inscricoes.forms import InscricaoForm
 
 
 def inscricao(request):
-    context = {'form': InscricaoForm()}
-    return render(request, 'inscricao_form.html', context)
+    if request.method == 'POST':
+        form = InscricaoForm(request.POST)
+        if form.is_valid():
+            context = dict(name = 'Vicente Luz', cpf = '12345678901',
+                           email = 'vicente.luz@armazemparaiba.com.br',
+                           phone = '86-98822-1812')
+            body = render_to_string('inscricao_email.txt', form.cleaned_data)
+            mail.send_mail('Confirmacao de Inscricao',
+                            body,
+                            'contato@eventex.com',
+                           ['contato@eventex.com', form.cleaned_data['email']])
+            messages.success(request, 'Inscrição realizada com sucesso!')
+
+            return HttpResponseRedirect('/inscricao/')
+        else:
+            return render(request, 'inscricao_form.html', {'form':form})
+    else:
+        context = {'form': InscricaoForm()}
+        return render(request, 'inscricao_form.html', context)
+
