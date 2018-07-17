fbbaa6ab430b90f4c09311d531f7010c8960375d
vicente.luz <vicenteoluz@gmail.com>
2018-07-17 16:00:28 -0300

    Comenta variavel MASK no settings

--- eventex/settings.py
+++ eventex/settings.py
@@ -17,7 +17,7 @@ from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MASK = config('MASK', cast=lambda s: int(s, 16))
+#MASK = config('MASK', cast=lambda s: int(s, 16))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
